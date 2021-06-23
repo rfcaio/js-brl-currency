872ba756a9d29ed0bea0400903f17c37bb1294ec
Caio Rodrigues <rfcaio90@gmail.com>
2021-06-23 20:39:00 -0300

    add currency formatter

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const toBrl = (value) => {
   if (value.length === 2) {
     return `0,${value}`
   }
+
+  const integerPartWithThousandSeparators = value
+    .slice(0, -2)
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+  const fractionalPart = value.slice(-2)
+  return `${integerPartWithThousandSeparators},${fractionalPart}`
 }
 
 module.exports = toBrl
--- test.js
+++ test.js
@@ -12,4 +12,15 @@ describe('toBrl', () => {
   test('autocompletes when a string with two characters is passed', () => {
     expect(toBrl('12')).toEqual('0,12')
   })
+
+  test('masks strings with more than two characters', () => {
+    expect(toBrl('12345')).toEqual('123,45')
+  })
+
+  test('adds thousands separator when a string with more than five characters is passed', () => {
+    expect(toBrl('123456')).toEqual('1.234,56')
+    expect(toBrl('1234567')).toEqual('12.345,67')
+    expect(toBrl('12345678')).toEqual('123.456,78')
+    expect(toBrl('123456789')).toEqual('1.234.567,89')
+  })
 })
