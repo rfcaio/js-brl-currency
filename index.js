41cde1ea6ee9cf846647b8c513937cc391d7d264
Caio Rodrigues <rfcaio90@gmail.com>
2021-06-23 18:20:07 -0300

    add one character string case

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const toBrl = (value) => {
   if (value.length === 0) {
     return '0,00'
   }
+
+  if (value.length === 1) {
+    return `0,0${value}`
+  }
 }
 
 module.exports = toBrl
--- test.js
+++ test.js
@@ -4,4 +4,8 @@ describe('toBrl', () => {
   test('returns `0,00` when an empty string is passed', () => {
     expect(toBrl('')).toEqual('0,00')
   })
+
+  test('autocompletes when an string with one character is passed', () => {
+    expect(toBrl('4')).toEqual('0,04')
+  })
 })
