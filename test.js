2de48abbbbff6fb90bf6133488f38a384df61477
Caio Rodrigues <rfcaio90@gmail.com>
2021-07-04 00:31:57 -0300

    improve tests structure

--- test.js
+++ test.js
@@ -5,22 +5,21 @@ describe('toBrl', () => {
     expect(toBrl('')).toEqual('0,00')
   })
 
-  test('autocompletes when a string with one character is passed', () => {
-    expect(toBrl('1')).toEqual('0,01')
-  })
-
-  test('autocompletes when a string with two characters is passed', () => {
-    expect(toBrl('12')).toEqual('0,12')
+  test('autocompletes when the first two characters are added', () => {
+    expect(toBrl('0,001')).toEqual('0,01')
+    expect(toBrl('0,012')).toEqual('0,12')
   })
 
   test('masks strings with more than two characters', () => {
-    expect(toBrl('12345')).toEqual('123,45')
+    expect(toBrl('0,123')).toEqual('1,23')
+    expect(toBrl('1,234')).toEqual('12,34')
+    expect(toBrl('12,345')).toEqual('123,45')
   })
 
   test('adds thousands separator when a string with more than five characters is passed', () => {
-    expect(toBrl('123456')).toEqual('1.234,56')
-    expect(toBrl('1234567')).toEqual('12.345,67')
-    expect(toBrl('12345678')).toEqual('123.456,78')
-    expect(toBrl('123456789')).toEqual('1.234.567,89')
+    expect(toBrl('123,456')).toEqual('1.234,56')
+    expect(toBrl('1.234,567')).toEqual('12.345,67')
+    expect(toBrl('12.345,678')).toEqual('123.456,78')
+    expect(toBrl('123.456,789')).toEqual('1.234.567,89')
   })
 })
